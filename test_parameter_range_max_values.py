807a180d519c3d49ad6ad648372ee41d6431f39d
Afshin134970 <afshien@gmail.com>
2023-11-14 12:24:12 -0800

    Updated the code to correctly verify Actual message from serial port against expected pre-defined values

--- test_parameter_range_max_values.py
+++ test_parameter_range_max_values.py
@@ -9,17 +9,20 @@ if current_user == '320027279':
     current_user = 'Afshin Ashrafy'
 else:
     current_user = getpass.getuser()
+
 # Define the serial port and baud rate
 ser = serial.Serial('COM5', 19200)
 
 # Define a list of tuples where each tuple contains a command and its expected response for C5 ParameterRange_MaxValues
-commands = [(bytes([0x84, 0x04, 0x01, 0x06, 0x52, 0x1F]), bytes([0x84, 0x04, 0x01, 0x06, 0x52, 0x1f])),
-            (bytes([0x84, 0x06, 0x0B, 0x4F, 0x02, 0x00, 0x00, 0x1A]), bytes([0x84, 0x06, 0x0B, 0x4F, 0x02, 0x00, 0x00, 0x1A])),
-            (bytes([0x84, 0x04, 0x04, 0x03, 0x74, 0x7D]), bytes([0x84, 0x04, 0x04, 0x03, 0x74, 0x7D])),
-            (bytes([0x84, 0x03, 0x09, 0x01, 0x6F]), bytes([0x84, 0x03, 0x09, 0x01, 0x6F])),
-            (bytes([0x88, 0x05, 0x55, 0x1E, 0x07, 0x7F, 0x7A]), bytes([0x88, 0x04, 0x1E, 0x07, 0x7F, 0x50])),
-            (bytes([0x88, 0x05, 0x55, 0x1F, 0x07, 0x7F, 0x79]), bytes([0x88, 0x04, 0x1F, 0x07, 0x7F, 0x4F])),
-            (bytes([0x88, 0x05, 0x55, 0x20, 0x07, 0x7F, 0x78]), bytes([0x88, 0x04, 0x20, 0x07, 0x7F, 0x4E]))]
+commands = [
+    (bytes([0x84, 0x04, 0x01, 0x06, 0x52, 0x1F]), bytes([0x84, 0x04, 0x01, 0x06, 0x52, 0x1F])),
+    (bytes([0x84, 0x06, 0x0B, 0x4F, 0x02, 0x00, 0x00, 0x1A]), bytes([0x84, 0x06, 0x0B, 0x4F, 0x02, 0x00, 0x00, 0x1A])),
+    (bytes([0x84, 0x04, 0x04, 0x03, 0x74, 0x7D]), bytes([0x84, 0x04, 0x04, 0x03, 0x74, 0x7D])),
+    (bytes([0x84, 0x03, 0x09, 0x01, 0x6F]), bytes([0x84, 0x03, 0x09, 0x01, 0x6F])),
+    (bytes([0x88, 0x05, 0x55, 0x1E, 0x07, 0x7F, 0x7A]), bytes([0x88, 0x04, 0x1E, 0x07, 0x7F, 0x50])),
+    (bytes([0x88, 0x05, 0x55, 0x1F, 0x07, 0x7F, 0x79]), bytes([0x88, 0x04, 0x1F, 0x07, 0x7F, 0x4F])),
+    (bytes([0x88, 0x05, 0x55, 0x20, 0x07, 0x7F, 0x78]), bytes([0x88, 0x04, 0x20, 0x07, 0x7F, 0x4E]))
+]
 
 # Initialize an empty list to store responses
 responses = []
@@ -27,11 +30,8 @@ responses = []
 # Get the current date and time
 current_datetime = datetime.datetime.now()
 
-
 # Create a dynamic output file name with timestamp
 timestamp_str = current_datetime.strftime('%b %m, %Y-%H-%M-%S')
-
-# Create a dynamic output file name with timestamp
 output_file_name = f'Param_MaxLimitCommands_Results_{timestamp_str}.txt'
 
 # Record the start time
@@ -43,23 +43,24 @@ with open(output_file_name, 'w') as file:
     file.write(f"{'*' * 50} Executed by: {current_user} {'*' * 76}\n")
     # Add the date and time of the run in the middle of the file
     file.write(f"{'*' * 50} Date and Time of Run: {current_datetime.strftime('%B %d, %Y %H:%M:%S')} {'*' * 51}\n\n")
-    # Send commands and receive responses
+
+    #Send commands and receive responses
     for cmd, expected_response in commands:
         ser.write(cmd)  # Send the command
         response = ser.read(len(expected_response))  # Read the expected number of bytes
         responses.append(response)
 
-        # Verify responses using assert and write to the output file
+    for i, (cmd, expected_response) in enumerate(commands):  # Use enumerate to get both index and values
         verification_message = (
-            f"Verifying command {' '.join([f'{byte:02X}' for byte in cmd])}, "
-            f"Received response {' '.join([f'{byte:02X}' for byte in response])} "
-            f"against expected response {' '.join([f'{byte:02X}' for byte in expected_response])}")
+            f"Command {' '.join([f'{byte:02X}' for byte in cmd])}, "
+            f"Expected Response {' '.join([f'{byte:02X}' for byte in expected_response])}, "
+            f"Actual Response {' '.join([f'{byte:02X}' for byte in responses[i]])}")
 
-        if response == expected_response:
+        if responses[i] == expected_response:
             verification_message += " --> PASS" + "\n"
         else:
             verification_message += " --> FAIL" + "\n"
-        file.write(verification_message + '\n')
+        file.write(str(verification_message) + '\n')  # Convert the tuple to a string before writing
 
 # Record the end time
 end_time = datetime.datetime.now()
